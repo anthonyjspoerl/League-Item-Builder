031bd68de8f2374a6253c607f12b167b547f7009
anthonyjspoerl <spoerla@uni.edu>
2015-11-18 22:16:41 -0600

    Test staticData.item error handling

--- src/collect-item-data.js
+++ src/collect-item-data.js
@@ -3,12 +3,17 @@ var riot = require('riot-games-api-nodejs');
 riot.developerKey = 'e3285489-6f06-47b3-9b7a-108971753268';
 riot.settings.region = 'na';
 
-function preloadItems(callback){
+var items;
+
+function getItems(callback){
+	if(items) return callback(null, items);
+
 	riot.staticData.item({itemListData: 'gold,image'}, function(err, data){
 		if(err) {
 			console.log(err);
-			callback(err);
+			return callback(err);
 		}
+		items = data;
 		callback(null, data);
 	});
 }
@@ -17,5 +22,5 @@ function calculateCost(item){
 
 }
 
-exports.preloadItems = preloadItems;
+exports.getItems = getItems;
 exports.calculateCost = calculateCost;
--- test/collect-item-data.js
+++ test/collect-item-data.js
@@ -1,17 +1,56 @@
-var assert = require('assert');
-var rewire = require('rewire');
+var assert  = require('assert');
+var sinon   = require('sinon');
+var rewire  = require('rewire');
+var chance  = require('chance');
 var collect = rewire('../src/collect-item-data');
 
 describe('collect-item-data', function(){
-	describe('#preloadItems(callback)', function(){
-		context('required itemListDetails', function(){
-			this.timeout(30000); // increase mocha default timeout for api call
-			
+	var sandbox = sinon.sandbox.create();
+	var riot    = collect.__get__('riot');
+
+	beforeEach(function(){
+		sandbox.restore();
+	});
+
+	describe('#getItems(callback)', function(){
+		context('avoid multiple api calls', function(){
+			// increase mocha default timeout for api call
+			// * unfortunately, if these tests fail, some/all others may timeout
+			this.timeout(30000);
+
+			it('should save item list from api call', function(done){
+				// when
+				collect.getItems(function(err, returnedItems){
+					// then
+					var savedItems = collect.__get__('items');
+					assert.equal(savedItems, returnedItems);
+					done();
+				});
+			});
+			it('should not make api call once list is saved', function(done){
+				// given
+				collect.__set__('items'); // reset items
+				var riotItemSpy = sandbox.spy(collect.__get__('riot.staticData'), 'item');
+
+				// when
+				// * this is very hacky/confusing, considering refactor with async
+				collect.getItems(function(){
+					assert.notEqual(typeof collect.__get__('items'), 'undefined');
+
+					collect.getItems(function(err, returnedItems){
+						// then
+						assert(riotItemSpy.calledOnce);
+						done();
+					});
+				});
+			});
+		});
+		context('required itemListDetails', function(){			
 			it('should load items with gold data', function(done){
 				// given
 
 				// when
-				collect.preloadItems(function(err, items){
+				collect.getItems(function(err, items){
 					// then
 					// 1001 apparently first item occurance
 					assert.notEqual(typeof items['1001'].gold, 'undefined');
@@ -22,7 +61,7 @@ describe('collect-item-data', function(){
 				// given
 
 				// when
-				collect.preloadItems(function(err, items){
+				collect.getItems(function(err, items){
 					// then
 					assert.notEqual(typeof items['1001'].image, 'undefined');
 					done();
@@ -30,11 +69,23 @@ describe('collect-item-data', function(){
 			});
 		});
 		context('error handling', function(){
+			it('should not save item data if there was an error', function(){
+				// given
+				collect.__set__('items', undefined);
+				sandbox.stub(riot.staticData, 'item', function(params, cb){
+					cb(new Error('No data for you'));
+				});
 
+				// when
+				collect.getItems(function(){});
+
+				// then
+				assert.equal(collect.__get__('items'), undefined);
+			});
 		});
 	});
 
 	describe('#calculateCost(items)', function(){
-		it('should return succesful from Riot API call');
+		it('should sum gold cost of all items');
 	});
 });
